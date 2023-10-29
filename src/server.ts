94f1f35a2dcdbe0ee6761c48bb2eac034cff3345
EriveltonDev <eriveltondesousa26@gmail.com>
2023-10-29 17:27:57 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 app.listen({
   port: env.PORT,
   host: '0.0.0.0'
 }).then(() => {
   console.log(`Server is running on port ${env.PORT}`)
+}).catch(() => {
+  console.error('Error on initialize the project')
 })
